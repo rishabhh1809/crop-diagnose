3333b3260596e0f72c609b8866d5d59f35ce6716
rishabh hadagali <135855891+rishabhh1809@users.noreply.github.com>
2026-04-16 01:21:49 +0000

    minor vite change

--- frontend/my-app/vite.config.ts
+++ frontend/my-app/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
+		allowedHosts: true, // Allow ngrok hosts to connect
 		proxy: {
 			// Proxy all /api HTTP requests to FastAPI during development.
 			// WebSocket proxy is handled separately below.
